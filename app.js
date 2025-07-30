a31c21b36c613e1e33dd32ed358b59f468e1f967
Tushar yadav <tushar10591@gmail.com>
2025-07-31 01:21:10 +0530

    server-side validation and error handled

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejsMate = require('ejs-mate');
 const path=require("path");
 const methodOverride = require('method-override')
 const wrapAsync=require("./utils/wrapAsync");
+const ExpressError=require("./utils/ExpressError");
+const {listingSchema}=require("./schema.js");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
@@ -20,21 +22,30 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join("");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
 app.get("/",(req,res)=>{
     res.send("working");
 })
 // index or home route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListing=await Listing.find();
     res.render("listings/index.ejs",{allListing});
-})
+}))
 
 //new route to create new listing
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new");
 })
 //post/create route to send created data in db from server form and then to show on home page by redirecting
-app.post("/listings",wrapAsync(async(req,res,next)=>{
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
     // let {title,description,price,location ,country,image}=req.body;
     // let newListing=new Listing({
     //     title,
@@ -44,40 +55,56 @@ app.post("/listings",wrapAsync(async(req,res,next)=>{
     //     country
     // })
     //method 2- listing object se name liye h uske andar object mein keys mein h saare already bas insert krdo vo object sidhe
-    
+    // if(!req.body.listing){
+    //     return next(new ExpressError(400,"Send Valid data for listing"));
+    //     // throw new ExpressError("Send Valid data for listing");
+    // }
+    // let result=listingSchema.validate(req.body);
+    // if(result.error){
+    //     return next(new ExpressError(400,result.error));
+    // }
     let newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 }))
 //show route to show specific data
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     console.log(id);
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-})
+}))
 // edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     console.log(listing);
     res.render("listings/edit",{listing})
-})
+}))
 //update route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
+    // if(!req.body.listing){
+    //     // return next(new ExpressError(404,"Send Valid data for listing"));
+    //     throw new ExpressError(400,"Send Valid data for listing");
+    // }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}))
 //delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deleteListing=await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-})
+}))
+app.use((req, res, next) => {
+    next(new ExpressError(404, "page not found"));
+});
 app.use((err,req,res,next)=>{
-    res.send("something went wrong")
+    let {statusCode=500,message="some error occured"}=err;
+    res.status(statusCode).render("error.ejs",{message});
+    // res.status(statusCode).send(message);
 })
 app.listen(port,()=>{
     console.log(`connection successfully established at ${port}`);
---
+++ schema.js
@@ -0,0 +1,13 @@
+const Joi = require("joi");
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        // Remove image from here
+    }).required(),
+    image: Joi.string().allow("").optional() // Place image here, at root level
+});
