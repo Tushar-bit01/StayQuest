524f38ab1b286a79df2800f888817c28c61126ee
Tushar yadav <tushar10591@gmail.com>
2025-08-17 20:54:41 +0530

    review deletion from both collection

--- app.js
+++ app.js
@@ -122,6 +122,13 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync( async(req,res)=>{
     
     res.render("feedbackthnx.ejs",{id});
 }));
+//delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}))
 app.use((req, res, next) => {
     next(new ExpressError(404, "page not found"));
 });
