2ed5dfcd54a0c7bf41c4788dddff054b202ad2b0
Tushar yadav <tushar10591@gmail.com>
2025-08-23 22:29:56 +0530

    post-login page

--- middleware.js
+++ middleware.js
@@ -1,8 +1,15 @@
 module.exports.isLoggedIn=(req,res,next)=>{
     console.log(req.user);
     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
         req.flash("error","User must be logged in to access this Features!");
-        return res.redirect("/login")
+        return res.redirect("/login");
+    }
+    next();
+}
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
 }
