716335286d290c5d0f451058a528979e36b4731e
Tushar yadav <tushar10591@gmail.com>
2025-08-24 23:04:25 +0530

    router route method implemented in users & listings

--- routes/listing.js
+++ routes/listing.js
@@ -4,20 +4,19 @@ const Listing=require("../models/listing");
 const wrapAsync=require("../utils/wrapAsync");
 const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 const listingController=require("../controllers/listing.js");
-// index or home route
-router.get("/",wrapAsync(listingController.index));
+router.route("/")
+.get(wrapAsync(listingController.index))// index or home route
+.post(isLoggedIn,validateListing,wrapAsync(listingController.createListing));//post/create route to send created data in db from server form and then to show on home page by redirecting
 
 //new route to create new listing
-router.get("/new",isLoggedIn,listingController.renderNewForm)
-//post/create route to send created data in db from server form and then to show on home page by redirecting
-router.post("/",isLoggedIn,validateListing,wrapAsync(listingController.createListing));
-//show route to show specific data
-router.get("/:id",wrapAsync(listingController.showListing));
+router.get("/new",isLoggedIn,listingController.renderNewForm);
+
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))//show route to show specific data
+.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))//update route
+.delete( isLoggedIn,isOwner, wrapAsync(listingController.deleteListing));//delete route
+
 //edit route
 router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
-//update route
-router.put("/:id",isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
-//delete route
-router.delete("/:id", isLoggedIn,isOwner, wrapAsync(listingController.deleteListing));
 
 module.exports=router;
